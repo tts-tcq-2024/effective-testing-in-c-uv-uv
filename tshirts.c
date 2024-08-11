07db508c2b9b7733b0047205180629c849629b3c
YUK5COB <K.Yuvaraj@in.bosch.com>
2024-08-11 17:32:01 +0530

    for L size

--- tshirts.c
+++ tshirts.c
@@ -18,6 +18,7 @@ int main() {
     assert(size(40) == 'M');
     assert(size(43) == 'L');
     assert(size(38) == 'M');
+    assert(size(42) == 'L');
     printf("All is well (maybe!)\n");
     return 0;
 }
